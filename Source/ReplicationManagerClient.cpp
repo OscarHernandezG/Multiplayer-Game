6580564622798740685b1f6b84d0f7bc961ee702
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-11-09 12:03:23 +0100

    Create ReplicationManagerClient
    With @aleixgab

--- Source/ReplicationManagerClient.cpp
+++ Source/ReplicationManagerClient.cpp
@@ -1,2 +1,71 @@
 #include "Networks.h"
+#include "ReplicationManagerClient.h"
 
+void ReplicationManagerClient::Read(const InputMemoryStream& packet)
+{
+	uint32 networkId; packet >> networkId;
+
+	ReplicationAction action; packet >> action;
+
+	switch (action)
+	{
+	case ReplicationAction::None:
+		break;
+	case ReplicationAction::Create:
+	{
+		GameObject* newGo = Instantiate();
+
+		if (newGo)
+		{
+			packet >> newGo->position.x;
+			packet >> newGo->position.y;
+
+			packet >> newGo->angle;
+
+			packet >> newGo->order;
+
+			packet >> newGo->pivot.x;
+			packet >> newGo->pivot.y;
+
+			packet >> newGo->size.x;
+			packet >> newGo->size.y;
+
+			packet >> newGo->color.r;
+			packet >> newGo->color.g;
+			packet >> newGo->color.b;
+			packet >> newGo->color.a;
+
+			packet >> newGo->tag;
+
+			App->modLinkingContext->registerNetworkGameObjectWithNetworkId(newGo, networkId);
+		}
+	}
+	break;
+	case ReplicationAction::Update:
+	{
+		GameObject* go = App->modLinkingContext->getNetworkGameObject(networkId);
+
+		if (go)
+		{
+			packet >> go->position.x;
+			packet >> go->position.y;
+					  
+			packet >> go->angle;
+
+		}
+	}
+	break;
+	case ReplicationAction::Destroy:
+		GameObject* go = App->modLinkingContext->getNetworkGameObject(networkId);
+
+		if (go)
+		{
+			App->modLinkingContext->unregisterNetworkGameObject(go);
+
+			Destroy(go);
+		}
+		break;
+	default:
+		break;
+	}
+}
