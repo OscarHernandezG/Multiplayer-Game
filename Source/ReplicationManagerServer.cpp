a76dbb2e76351d56d7970ec62ac7db908294bce6
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-11-09 12:03:06 +0100

    Create ReplicationManagerServer
    With @aleixgab

--- Source/ReplicationManagerServer.cpp
+++ Source/ReplicationManagerServer.cpp
@@ -1,2 +1,76 @@
 #include "Networks.h"
+#include "ReplicationManagerServer.h"
 
+using namespace std;
+
+void ReplicationManagerServer::create(uint32 networkId)
+{
+	commandList.push_back(new ReplicationCommand(ReplicationAction::Create, networkId));
+}
+
+void ReplicationManagerServer::update(uint32 networkId)
+{
+	commandList.push_back(new ReplicationCommand(ReplicationAction::Update, networkId));
+}
+
+void ReplicationManagerServer::destroy(uint32 networkId)
+{
+	commandList.push_back(new ReplicationCommand(ReplicationAction::Create, networkId));
+}
+
+
+
+void ReplicationManagerServer::write(OutputMemoryStream& packet)
+{
+	for (list<ReplicationCommand*>::iterator it = commandList.begin(); it != commandList.end(); ++it)
+	{
+		packet << (*it)->networkId;
+		packet << (*it)->action;
+
+		GameObject* go = App->modLinkingContext->getNetworkGameObject((*it)->networkId);
+		switch ((*it)->action)
+		{
+		case ReplicationAction::None:
+			break;
+		case ReplicationAction::Create:
+			packet << go->position.x;
+			packet << go->position.y;
+
+			packet << go->angle;
+
+			packet << go->order;
+
+			packet << go->pivot.x;
+			packet << go->pivot.y;
+
+			packet << go->size.x;
+			packet << go->size.y;
+
+			packet << go->color.r;
+			packet << go->color.g;
+			packet << go->color.b;
+			packet << go->color.a;
+
+			packet << go->tag;
+
+			break;
+		case ReplicationAction::Update:
+			packet << go->position.x;
+			packet << go->position.y;
+
+			packet << go->angle;
+
+			break;
+		case ReplicationAction::Destroy:
+			break;
+		default:
+			break;
+		}
+
+		delete *it;
+		*it = nullptr;
+	}
+
+	commandList.clear();
+
+}
--- Source/ReplicationManagerServer.h
+++ Source/ReplicationManagerServer.h
@@ -1,2 +1,33 @@
 #pragma once
+#include <list>
 
+
+enum class ReplicationAction
+{ 
+	None,
+	Create,
+	Update, 
+	Destroy
+};
+
+struct ReplicationCommand
+{
+	ReplicationCommand(ReplicationAction action, uint32 networkId) : action(action), networkId(networkId) {}
+
+	ReplicationAction action = ReplicationAction::None;
+	uint32 networkId = 0u;
+};
+
+class ReplicationManagerServer 
+{
+public:
+	void create(uint32 networkId);
+	void update(uint32 networkId);
+	void destroy(uint32 networkId);
+
+	void write(OutputMemoryStream& packet);
+	// More members... 
+
+public:
+	std::list<ReplicationCommand*> commandList;
+};
