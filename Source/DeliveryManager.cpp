94ab2051ca017157486c8ad36fd35c199e4ae141
aleixgab <aleix.gab@gmail.com>
2019-11-28 11:53:47 +0100

    Delivery Manager and Delegate class

--- Source/DeliveryManager.cpp
+++ Source/DeliveryManager.cpp
@@ -1,2 +1,32 @@
 #include "Networks.h"
 
+Delivery* DeliveryManager::WriteSequenceNumber(OutputMemoryStream& packet)
+{
+	return nullptr;
+}
+
+bool DeliveryManager::ProcessSequenceNumber(const InputMemoryStream& packet)
+{
+	return false;
+}
+
+bool DeliveryManager::HasSequenceNumbersPendingAck() const
+{
+	return false;
+}
+
+void DeliveryManager::WritesequenceNumbersPendingAck(OutputMemoryStream& packet)
+{
+}
+
+void DeliveryManager::ProcessAckdSequenceNumbers(const InputMemoryStream& packet)
+{
+}
+
+void DeliveryManager::ProcessTimedOutPackets()
+{
+}
+
+void DeliveryManager::Clear()
+{
+}
--- Source/DeliveryManager.h
+++ Source/DeliveryManager.h
@@ -1,2 +1,52 @@
 #pragma once
 
+class DeliveryManager;
+
+class DeliveryDelegate
+{
+public:
+	virtual void OnDeliverySuccess(DeliveryManager* deliveryManager) = 0;
+	virtual void OnDeliveryFailure(DeliveryManager* deliveryManager) = 0;
+
+};
+struct Delivery
+{
+	uint32 sequenceNumber = 0;
+	double dispatchTime = 0.0;
+	DeliveryDelegate* delegate = nullptr;
+};
+
+
+
+class DeliveryManager
+{
+public:
+public:
+
+	// For senders to write a new seq. numbers into a packet 
+	Delivery* WriteSequenceNumber(OutputMemoryStream& packet);
+
+	// For receivers to process the seq. number from an incoming packet 
+	bool ProcessSequenceNumber(const InputMemoryStream& packet);
+
+	// For receivers to write ack'ed seq. numbers into a packet 
+	bool HasSequenceNumbersPendingAck() const;
+	void WritesequenceNumbersPendingAck(OutputMemoryStream& packet);
+
+	// For senders to process ack'ed seq. numbers from a packet
+	void ProcessAckdSequenceNumbers(const InputMemoryStream& packet);
+	void ProcessTimedOutPackets();
+
+	void Clear();
+
+private:
+
+	// Private members (sender side)
+	// - The next outgoing sequence number 
+	// - A list of pending deliveries
+
+	// Private members (receiver side)
+	// - The next expected sequence number
+	// - A list of sequence numbers pending ack
+
+};
